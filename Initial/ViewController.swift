d04bffb3ee2b8e5e4b4a225e01ee2946ad631d29
M Salah <m.salah@posrocket.com>
2016-10-04 09:19:35 +0300

    Initial commit

---
+++ Initial/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Initial
+//
+//  Created by Mohammad Salah on 10/3/16.
+//  Copyright © 2016 Mohammad Salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        test()
+        
+        var st = "Hello"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+
+    func test(){
+        print("")
+    }
+}
+
